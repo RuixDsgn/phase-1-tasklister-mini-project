c763e7a1f2ac2fc86c7965603271cec04641f664
Carla <ruixdsgn@gmail.com>
2023-05-02 23:36:41 -0700

    finished the first three deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => { //adds an event listener to the submit button 
+       e.preventDefault()
+       buildTask(e.target.new_task_description.value) //id was previously labeled new-task-description, but was causing an unknown syntax error // gets the string value of the new task
+       form.reset 
+  })
+})
+
+
+// Create a new element to store the value we captured from the eventListener
+function buildTask(task) {
+  let li = document.createElement('li') // appends the string value to a li element
+  console.log(li.className)
+  let deleteButton = document.createElement('button') // creates button element
+  document.addEventListener('click', handleDeleteButton)
+  deleteButton.textContent = "x"
+  li.textContent = `${task} `
+  li.appendChild(deleteButton) // appends the created button as a child to the parent element li
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li) //appends the li element as a child under the parent element ul
+  
+}
+
+function handleDeleteButton() {
+  const unorderedList = document.querySelector('ul')
+  const list = document.querySelector('li')
+  list.className = 'taskList'
+  unorderedList.removeChild(list)
+}
